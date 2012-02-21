b868fcf9564794631336d0d9edcfe84306d6ea2c
DrMoriarty <drmoriarty.0@gmail.com>
2012-02-21 16:03:10 +0800

    bugfix

--- Classes/ActiveView.m
+++ Classes/ActiveView.m
@@ -11,6 +11,7 @@
 #import "tubeAppDelegate.h"
 
 @implementation ActiveView
+@synthesize cityMap;
 
 + (Class)layerClass
 {
@@ -21,14 +22,17 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        tubeAppDelegate *appDelegate = 	(tubeAppDelegate *)[[UIApplication sharedApplication] delegate];
-        cityMap = appDelegate.cityMap;
         [self.layer setLevelsOfDetail:5];
         [self.layer setLevelsOfDetailBias:2];
-        self.frame = CGRectMake(0, 0, cityMap.w, cityMap.h);
         self.opaque = NO;
     }
     return self;
+}
+
+-(void)setCityMap:(CityMap *)_cityMap
+{
+    cityMap = _cityMap;
+    self.frame = CGRectMake(0, 0, cityMap.w, cityMap.h);
 }
 
 -(void) setHidden:(BOOL)hidden
@@ -38,16 +42,12 @@
         [self.layer invalidateContents];
         [self setNeedsDisplay];
         super.hidden = NO;
-        //self.alpha = 0;
-        //[UIView animateWithDuration:0.75f animations:^(void) { self.alpha = 1.f; } ];
     } else super.hidden = hidden;
 }
 
 - (void)drawLayer:(CALayer *)layer inContext:(CGContextRef)context {
     
     CGRect r = CGContextGetClipBoundingBox(context);
-    //CGContextSetFillColorWithColor(context, [[UIColor colorWithRed:1.f green:1.f blue:1.f alpha:0.75f] CGColor]);
-    //CGContextFillRect(context, r);
     if(!CGRectIntersectsRect(cityMap.activeExtent, r)) return;
     CGContextSaveGState(context);
     
