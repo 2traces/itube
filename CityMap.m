034e66321ecbd9ecabddaa7cd8faa85e503fc063
DrMoriarty <drmoriarty.0@gmail.com>
2012-04-19 19:33:38 +0400

    new parameter GpsMarkScale

--- CityMap.m
+++ CityMap.m
@@ -1545,6 +1545,7 @@ void drawFilledCircle(CGContextRef context, CGFloat x, CGFloat y, CGFloat r) {
 @synthesize currentScale;
 @synthesize backgroundImageFile;
 @synthesize foregroundImageFile;
+@synthesize gpsCircleScale;
 
 -(StationKind) stationKind { return StKind; }
 -(void) setStationKind:(StationKind)stationKind { StKind = stationKind; }
@@ -1563,6 +1564,7 @@ void drawFilledCircle(CGContextRef context, CGFloat x, CGFloat y, CGFloat r) {
     LineWidth = 4.f;
     StationDiameter = 8.f;
     FontSize = 7.f;
+    gpsCircleScale = 5.f;
     StKind = LIKE_PARIS;
     TrKind = LIKE_PARIS;
     TEXT_FONT = @"Arial-BoldMT";
@@ -1722,6 +1724,10 @@ void drawFilledCircle(CGContextRef context, CGFloat x, CGFloat y, CGFloat r) {
         maxScale = sc;
         PredrawScale = maxScale;
     }
+    sc = [[parserMap get:@"GpsMarkScale" section:@"Options"] floatValue];
+    if(sc != 0.f) {
+        gpsCircleScale = sc;
+    }
     BOOL tuneEnabled = [[parserMap get:@"TuneTransfers" section:@"Options"] boolValue];
 	
 	_w = 0;
@@ -1847,6 +1853,10 @@ void drawFilledCircle(CGContextRef context, CGFloat x, CGFloat y, CGFloat r) {
         PredrawScale = maxScale;
     }
     BOOL tuneEnabled = [[parserMap get:@"TuneTransfers" section:@"Options"] boolValue];
+    sc = [[parserMap get:@"GpsMarkScale" section:@"Options"] floatValue];
+    if(sc != 0.f) {
+        gpsCircleScale = sc;
+    }
 	
 	_w = 0;
 	_h = 0;
