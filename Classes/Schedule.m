38f12290b8188836cf9ba95f76a6819878bb6549
DrMoriarty <drmoriarty.0@gmail.com>
2012-04-21 21:22:15 +0400

    bugfix

--- Classes/Schedule.m
+++ Classes/Schedule.m
@@ -488,12 +488,14 @@ NSCharacterSet *pCharacterSet = nil;
                 [np setWeightFrom:p];
                 [propagate addObject:np];
             }
+            float curTime = np.time;
             if([flag valueForKey:np.name] == nil) {
                 [flag setValue:@"YES" forKey:np.name];
                 for (NSString *ln in lines) {
                     SchLine *l = [lines valueForKey:ln];
                     NSArray *sts = [l.catalog valueForKey:np.name];
                     for (SchPoint *tp in sts) {
+                        if(tp.time <= curTime) continue;
                         if(tp == np) continue;
                         if(tp.backPath != nil) [tp setWeightFrom:np];
                         else {
