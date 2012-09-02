eee4d8ca28e4f25fbdb239833fa3527ef8d2f1d5
DrMoriarty <drmoriarty.0@gmail.com>
2012-09-02 12:08:46 +0400

    gl gestures

---
+++ Classes/gl/GlViewController.h
@@ -0,0 +1,14 @@
+//
+//  GlViewController.h
+//  test
+//
+//  Created by Vasiliy Makarov on 22.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface GlViewController : GLKViewController
+
+@end
