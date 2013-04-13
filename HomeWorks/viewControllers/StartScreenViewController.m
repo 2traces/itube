812778f224c186d5484f007ce2e7c2c160401812
Sergey Egorov <bsideup@gmail.com>
2013-04-13 15:10:42 +0300

    catalog replacement test

--- HomeWorks/viewControllers/StartScreenViewController.m
+++ HomeWorks/viewControllers/StartScreenViewController.m
@@ -62,7 +62,8 @@
 		if ([rxml attribute:@"baseurl"])
 		{
 			[self.catalogRxml initFromXMLData:operation.responseData];
-			[operation.responseString writeToFile:self.catalogFilePath atomically:YES encoding:NSUTF8StringEncoding error:nil];
+			[operation.responseString writeToFile:self.catalogFilePath atomically:YES encoding:operation.responseStringEncoding error:nil];
+			NSLog(@"new catalog is: %@", [NSString stringWithContentsOfFile:self.catalogFilePath encoding:operation.responseStringEncoding error:nil]);
 		}
 
 		[self prepareProducts];
